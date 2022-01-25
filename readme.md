662cf0798ae09cd62ac5cd64626b1811d0e587be
Daniel Collione <danielcollione@MacBook-Air-de-suporte.local>
2022-01-25 17:32:07 -0300

    commit dia 25/1

---
+++ readme.md
@@ -0,0 +1,10 @@
+/**
+ * Params type:
+ * Routes Params => http://localhost:3000/produtos/12831928319
+ * Query Params => http://localhost:3000/produtos?name=teclado&description=tecladobom&parametro=valor
+ * 
+ * Body Params => {
+ *  "name": "keyboard",
+ *  "description": "good keyboard"
+ * }
+ */
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,9 @@
+import "reflect-metadata";
 import express from "express";
 
+import "./database";
+
 const app = express();
-
-app.get("/get", (req, res) => {
-  res.send("Rota GET");
-});
-
-app.post("/post", (req, res) => {
-  res.send("Rota Post");
-});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
